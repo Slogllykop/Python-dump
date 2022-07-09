4fcab4049dc71cc009fa2eda10ce1e2bbbaf9e9c
Slogllykop <slogllykop07@gmail.com>
2022-07-10 00:11:21 +0530

    100DOCC-10

---
+++ 100 days of code/Day 10.py
@@ -0,0 +1,8 @@
+# https://www.codechef.com/submit/WATERREQ
+
+t=int(input())
+if t>=1 and t<=1000:
+    for i in range(t):
+        n=int(input())
+        if n>=1 and n<=1000:
+            print(n*2)
